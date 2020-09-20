203c5a4dd511f958e80acc1565ba8b3426c5e749
imkrrish <imkrrish227@gmail.com>
2020-09-20 21:23:49 +0530

    created gameover and gameannounce function

--- main.cpp
+++ main.cpp
@@ -7,9 +7,12 @@ class gameboard{
     int choice;
     char Turn = 'X';
     int row,col;
+    bool draw = false;
     public:
     void displayBoard();
     void playerTurn();
+    bool gameOver();
+    void announceWinner();
 
 };
 
@@ -61,7 +64,7 @@ void gameboard::playerTurn(){
         Turn = 'O';
     }
 
-    else if (Turn == 'O' && A[row][col] != 'X' && A[row][col] != 'O'){
+    else if(Turn == 'O' && A[row][col] != 'X' && A[row][col] != 'O'){
             A[row][col] = 'O';
             Turn = 'X';
     }
@@ -71,14 +74,67 @@ void gameboard::playerTurn(){
     }
     this->displayBoard();
 }
+
+bool gameboard::gameOver(){
+//check winner
+    for(int i = 0; i < 3; i++){
+        if( A[i][0] == A[i][1] && A[i][0] == A[i][2] || A[0][i] == A[1][i] && A[0][i] == A[2][i] ){
+            return false;
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+    if( A[0][0] == A[1][1] && A[0][0] == A[2][2] || A[0][2] == A[1][1] && A[0][2] == A[2][0] ){
+        return false;
+        }
+    }
+
+//game is running
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if (A[i][j] != 'X' && A[i][j] != 'O')
+            {
+                return true;
+            }
+            
+        }
+    }
+
+//draw game
+draw = true;
+return false;
+
+}
+
+void gameboard::announceWinner(){
+
+    if( Turn == 'O' && draw == false){
+        cout<<"\n\n\t\t*******Congratulations*******\n";
+        cout<<"\n\t\t     Player1 [X] Wins !!!\n\n";
+    }
+
+    else if( Turn == 'X' && draw == false){
+        cout<<"\n\n\t\t*******Congratulations*******\n";
+        cout<<"\n\t\t     Player2 [O] Wins !!!\n\n";
+    }
+    
+    else{
+        cout<<"\n\n\t\t*** G A M E   D R A W ***\n";
+    }
+
+}
+
+
 int main(){
     gameboard obj;
 
-    while (true)
+    while (obj.gameOver())
     {
         obj.displayBoard();
         obj.playerTurn();
+        obj.gameOver();
     }
-    
+
+    obj.announceWinner();
     return 0;
 }
